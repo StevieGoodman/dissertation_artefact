fb25c78b1e95a87b5fc687f1d0fea0a16313736b
Stevie Goodman <mail@steviegoodman.com>
2023-12-19 15:44:19 +0000

    Added `IdentityService`

---
+++ src/ServerScriptService/Knit/Services/IdentityService.lua
@@ -0,0 +1,58 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local TableUtil = require(ReplicatedStorage.Packages.TableUtil)
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+local SURNAMES = {
+    "Smith", "Brown", "Wilson", "Thomson", "Robertson", "Campbell", "Stewart",
+    "Anderson", "MacDonald", "Scott", "Reid", "Murray", "Taylor", "Clark",
+    "Mitchell", "Ross", "Walker", "Paterson", "Young", "Watson", "Morrison",
+    "Fraser", "Davidson", "Gray",
+}
+
+local service = Knit.CreateService {
+    Name = "Identity",
+}
+
+function service:KnitInit()
+    self.surnames = {}
+    for _, surname in SURNAMES do
+        self.surnames[surname] = false
+    end
+end
+
+function service:isSurnameAvailable(surname: string)
+    return self.surnames[surname] == false
+end
+
+function service:getPlayerSurname(player: Player)
+    local _, playerSurname = TableUtil.Find(self.surnames, function(otherPlayer)
+        return player == otherPlayer
+    end)
+    return playerSurname
+end
+
+function service:reserveSurname(player: Player)
+    local playerSurname = self:getPlayerSurname(player)
+    if playerSurname then
+        error(`{player} ({playerSurname}) already has a surname!`)
+    else
+        local _, surname = TableUtil.Find(self.surnames, function(_, key)
+            return self:isSurnameAvailable(key)
+        end)
+        self.surnames[surname] = player
+        return surname
+    end
+end
+
+function service:releaseSurname(player: Player)
+    self.surnames = TableUtil.Map(self.surnames, function(_, key)
+        if key == player then
+            return false
+        else
+            return key
+        end
+    end)
+end
+
+return service
