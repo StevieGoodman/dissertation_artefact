ed1bd784feed5c485416ae6b92ce956443b69e58
Stevie Goodman <mail@steviegoodman.com>
2023-12-22 15:08:50 +0000

    Added `SCP008` and `SCP008Infection` components

---
+++ src/ServerScriptService/Cmdr/Commands/InfectSCP008/InfectSCP008.lua
@@ -0,0 +1,12 @@
+return {
+    Name = "infect008",
+    Description = "Infects a player(s) with SCP-008",
+    Group = "development",
+    Args = {
+        {
+            Type = "players",
+            Name = "target",
+            Description = "The players to infect",
+        },
+    },
+}
---
+++ src/ServerScriptService/Component/SCPs/SCP008Infection.lua
@@ -0,0 +1,55 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Component = require(ReplicatedStorage.Packages.Component)
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+local DAMAGE_RATE = 10
+local PROGRESSION_RATE_MIN = 5
+local PROGRESSION_RATE_MAX = 10
+
+local component = Component.new {
+    Tag = "SCP008Infection",
+    Ancestors = { workspace }
+}
+
+function component:Construct()
+    Knit.OnStart():await()
+    self.assetService = Knit.GetService("Asset")
+    self.progression = 0
+    self.progressionRate = math.random(PROGRESSION_RATE_MIN, PROGRESSION_RATE_MAX)
+    self.contagious = false
+    self.humanoidRoot = self.Instance
+    self.humanoid = self.Instance.Parent.Humanoid :: Humanoid
+end
+
+function component:Start()
+    print(`{self.Instance.Parent.Name} has been infected by SCP-008!`)
+end
+
+function component:SteppedUpdate(dt)
+    self.progression = self.progression + (self.progressionRate * dt)
+    self.progression = math.clamp(self.progression, 0, 100)
+    if self.progression == 100 then
+        self.humanoid:TakeDamage(DAMAGE_RATE * dt)
+    end
+    if self.progression >= 30 and not self.contagious then
+        self:makeContagious()
+    end
+end
+
+function component:makeContagious()
+    self.contagious = true
+    local description = self.assetService:getAsset("SCP008HumanoidDescription") :: HumanoidDescription
+    local currentDescription = self.humanoid:GetAppliedDescription()
+    local currentAccessories = currentDescription:GetAccessories(true)
+    description:SetAccessories(currentAccessories, true)
+    description.Shirt = currentDescription.Shirt
+    description.Pants = currentDescription.Pants
+    if description then
+        self.humanoid:ApplyDescription(description)
+    end
+    self.humanoidRoot:AddTag("SCP008")
+end
+
+
+return component
