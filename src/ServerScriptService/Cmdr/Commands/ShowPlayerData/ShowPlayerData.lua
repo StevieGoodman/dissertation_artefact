4e908597eefd3633186bf2eee23bed8884c1ba6b
Stevie Goodman <mail@steviegoodman.com>
2024-04-16 21:26:25 +0100

    Added ShowPlayerData command

---
+++ src/ServerScriptService/Cmdr/Commands/ShowPlayerData/ShowPlayerData.lua
@@ -0,0 +1,12 @@
+return {
+    Name = "showPlayerData",
+    Description = "Shows a player's loaded data",
+    Group = "development",
+    Args = {
+        {
+            Type = "player",
+            Name = "target",
+            Description = "Whose data to view",
+        },
+    },
+}
---
+++ src/ServerScriptService/Cmdr/Commands/ShowPlayerData/ShowPlayerDataServer.lua
@@ -0,0 +1,33 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Knit = require(ReplicatedStorage.Packages.Knit)
+
+function parseData(data, indent: number?)
+    if not indent then
+        indent = 0
+    end
+    -- Parse data
+    local parsedData = "{\n"
+    for key, value in data do
+        parsedData ..= string.rep(" ", indent + 4)
+        if type(value) == "table" then
+            value = parseData(value, indent + 4)
+        end
+        parsedData ..= `{key}: {value}\n`
+    end
+    parsedData ..= string.rep(" ", indent) .. "}\n"
+    parsedData = string.sub(parsedData, 1, -2)
+    return parsedData
+end
+
+return function(_, player: Player)
+    local success, result = pcall(function()
+        return Knit.GetService("PlayerData"):getProfileData(player)
+    end)
+    if success then
+        local message = parseData(result)
+        return message
+    else
+        return `Failed to get player data! {result}`
+    end
+end
