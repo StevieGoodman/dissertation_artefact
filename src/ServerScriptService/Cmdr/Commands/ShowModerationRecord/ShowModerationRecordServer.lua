d50aa7363f87cbf981bc6615684177a08e39d929
Stevie Goodman <mail@steviegoodman.com>
2024-04-16 23:38:54 +0100

    Adjusted list

--- src/ServerScriptService/Cmdr/Commands/ShowModerationRecord/ShowModerationRecordServer.lua
+++ src/ServerScriptService/Cmdr/Commands/ShowModerationRecord/ShowModerationRecordServer.lua
@@ -5,30 +5,29 @@ local Knit = require(ReplicatedStorage.Packages.Knit)
 
 local ModerationService = Knit.GetService("Moderation")
 
+local TIME_FORMAT_STRING = "%A %d %B %Y at %I:%M%p"
+
 return function(_, player: Player)
     local moderationRecord = ModerationService:getModerationRecord(player)
     local message = ""
     for id, moderationEvent in moderationRecord do
-        local moderatorName = "Unknown"
-        message ..= `Event ID: {id}\n`
-        message ..= `Event Type: {moderationEvent.eventType}\n`
-        message ..= `Moderator: {moderatorName} ({moderationEvent.moderator})\n`
-        message ..= `Reason: {moderationEvent.reason}\n`
-        local formattedTime = os.date("!%A %d %B at %I:%M%p", moderationEvent.timestamp)
-        message ..= `Timestamp: {formattedTime}\n`
+        local moderatorName = `user ID {moderationEvent.moderator}`
+        pcall(function()
+            moderatorName = Players:GetNameFromUserIdAsync(moderationEvent.pardon.moderator)
+        end)
+        local formattedTime = os.date(TIME_FORMAT_STRING, moderationEvent.timestamp)
+        message ..= `(#{id}) {moderationEvent.eventType}: {moderationEvent.reason}\nModerated by {moderatorName} on {formattedTime}\n`
         if moderationEvent.pardon then
-            moderatorName = "Unknown"
+            moderatorName = `user ID {moderationEvent.pardon.moderator}`
             pcall(function()
                 moderatorName = Players:GetNameFromUserIdAsync(moderationEvent.pardon.moderator)
             end)
-            message ..= `Pardoned by: {moderatorName} ({moderationEvent.pardon.moderator})\n`
-            message ..= `Pardon Reason: {moderationEvent.pardon.reason}\n`
-            formattedTime = os.date("!%A %d %B at %I:%M%p", moderationEvent.pardon.timestamp)
-            message ..= `Pardon Timestamp: {formattedTime}\n`
+            formattedTime = os.date(TIME_FORMAT_STRING, moderationEvent.pardon.timestamp)
+            message ..= `Pardoned: {moderationEvent.pardon.reason}\nPardoned by {moderatorName} on {formattedTime}\n`
         end
-        message ..= "----------------\n"
+        message ..= "\n"
     end
-    message = string.sub(message, 1, -18) -- Remove the last line break
+    message = string.sub(message, 1, -3) -- Remove the last line break
     if message == "" then
         message = `{player.Name} has no moderation record.`
     end
