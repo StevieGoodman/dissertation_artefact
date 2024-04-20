d93a0fc6e82bf1b1bade1a00d9e4a1eb072d59a5
Stevie Goodman <mail@steviegoodman.com>
2024-04-20 14:16:40 +0100

    Added advanced interaction permissions

--- src/Server/Knit/Services/InteractionPermissionsService.lua
+++ src/Server/Knit/Services/InteractionPermissionsService.lua
@@ -1,13 +1,24 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
+local Waiter = require(ReplicatedStorage.Packages.WaiterV5)
 
 local service = Knit.CreateService {
     Name = "InteractionPermissions",
 }
 
-function service:checkPermissions(player: Player)
-    return player.Team.Name ~= "Class-D Personnel" or player:GetAttribute("InteractionPermissionsOverride")
+function service:getKeycardType(player: Player)
+    local keycard = Waiter.get.child(player.Backpack, "Keycard")
+    or Waiter.get.child(player.Character, "Keycard")
+    return if keycard then keycard:GetAttribute("KeycardType") else nil
+end
+
+function service:checkPermissions(player: Player, instance: Instance)
+    if player:GetAttribute("InteractionPermissionsOverride") then
+        return true
+    else
+        return instance:GetAttribute(`{self:getKeycardType(player)}Authorised`)
+    end
 end
 
 function service.Client:checkPermissions(player: Player)
