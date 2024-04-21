bbe5d6fc363b5ab8d5d306f2d3498c9e7e0c62d1
Stevie Goodman <mail@steviegoodman.com>
2024-04-21 20:26:33 +0100

    Updated help command

--- src/Server/Cmdr/Commands/Help/HelpServer.lua
+++ src/Server/Cmdr/Commands/Help/HelpServer.lua
@@ -1,12 +1,12 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Cmdr = require(ReplicatedStorage.Packages.Cmdr)
+
 return function(_)
-    return
-`[development] ReassignIdentity: A trans person's best friend! Modifies a character's identity.\
-[development] GetIdentities: Displays a list of all currently registered identities.\
-[development] Infect008: Infects a player with an instance of SCP-008.\
-[development] OverrideInteractionPermissions: Overrides a player's interaction permissions.\
-[development] GetPlayerData: Displays a player's loaded data.\
-[moderation] TeleportLocation: Teleports a player to a location in the site.\
-[moderation] Kick: Kicks players from the server.\
-[moderation] GetModerationRecord: Shows the moderation record of a player.\
-[moderation] Pardon: Pardons a moderation event.`
+    local commands = Cmdr.Registry:GetCommands()
+    local response = ""
+    for _, info in commands do
+        response ..= `[{info.Group or "player"}] {info.Name}: {info.Description}\n`
+    end
+    return string.sub(response, 1, -2)
 end
