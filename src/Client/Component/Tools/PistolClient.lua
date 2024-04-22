d825b7e06661983b4ae4bd88b5150d6db934f4c0
Stevie Goodman <mail@steviegoodman.com>
2024-04-22 23:26:34 +0100

    Added pistol aim animation

--- src/Client/Component/Tools/PistolClient.lua
+++ src/Client/Component/Tools/PistolClient.lua
@@ -5,6 +5,7 @@ local UserInputService = game:GetService("UserInputService")
 
 local Component = require(ReplicatedStorage.Packages.Component)
 local Knit = require(ReplicatedStorage.Packages.Knit)
+local Waiter = require(ReplicatedStorage.Packages.WaiterV5)
 
 local PLAYER = Players.LocalPlayer
 
@@ -15,37 +16,63 @@ local component = Component.new {
 function component:Construct()
     self.cursorController = Knit.GetController("Cursor")
     self.cameraController = Knit.GetController("Camera")
+    self.aimAnimation = Waiter.get.child(self.Instance, "AimAnimation")
+    assert(self.aimAnimation, "AimAnimation not found!")
     Knit.OnStart():await()
 end
 
 function component:Start()
+    print(not self.Instance:IsDescendantOf(PLAYER)
+    and not self.Instance:IsDescendantOf(PLAYER.Character))
     if not self.Instance:IsDescendantOf(PLAYER)
-        and not self.Instance:IsDescendantOf(PLAYER.Character) 
+        and not self.Instance:IsDescendantOf(PLAYER.Character)
         then return end
     self.Instance.Activated:Connect(function()
         self:tryFire()
     end)
     self.Instance.Equipped:Connect(function()
-        self.cursorController:setCursorIcon(self.cursorController.CursorIcon.Simple)
-        self.cameraController:setCameraType(self.cameraController.CameraType.CharacterLocked)
-        ContextActionService:BindAction("Reload", function(_, inputState, _)
-            if inputState == Enum.UserInputState.Begin then
-                self:reload()
-            end
-        end, false, Enum.KeyCode.R)
+        self:onEquip()
     end)
     self.Instance.Unequipped:Connect(function()
-        local cameraType =
-            if PLAYER.Character.Humanoid.Health <= 0
-            then self.cameraController.CameraType.CursorUnlocked
-            else self.cameraController.CameraType.CharacterUnlocked
-        self.cameraController:setCameraType(cameraType)
-        local cursorIcon =
-            if Players.LocalPlayer.Character.Humanoid.Health <= 0
-            then self.cursorController.CursorIcon.Default
-            else self.cursorController.CursorIcon.Hidden
-        self.cursorController:setCursorIcon(cursorIcon)
-        ContextActionService:UnbindAction("Reload")
+        self:onUnequip()
+    end)
+end
+
+function component:onEquip()
+    self.cursorController:setCursorIcon(self.cursorController.CursorIcon.Simple)
+    self.cameraController:setCameraType(self.cameraController.CameraType.CharacterLocked)
+    self:setUpAnimations()
+    ContextActionService:BindAction("Reload", function(_, inputState, _)
+        if inputState == Enum.UserInputState.Begin then
+            self:reload()
+        end
+    end, false, Enum.KeyCode.R)
+end
+
+function component:onUnequip()
+    local cameraType =
+        if PLAYER.Character.Humanoid.Health <= 0
+        then self.cameraController.CameraType.CursorUnlocked
+        else self.cameraController.CameraType.CharacterUnlocked
+    self.cameraController:setCameraType(cameraType)
+    local cursorIcon =
+        if Players.LocalPlayer.Character.Humanoid.Health <= 0
+        then self.cursorController.CursorIcon.Default
+        else self.cursorController.CursorIcon.Hidden
+    self.cursorController:setCursorIcon(cursorIcon)
+    ContextActionService:UnbindAction("Reload")
+end
+
+function component:setUpAnimations()
+    local character = PLAYER.Character
+    if not character then return end
+    local animator = character.Humanoid.Animator
+    if not animator then return end
+    self.aimAnimationTrack = animator:LoadAnimation(self.aimAnimation)
+    self.aimAnimationTrack.Looped = true
+    self.aimAnimationTrack:Play()
+    self.Instance.Unequipped:Connect(function()
+        self.aimAnimationTrack:Stop()
     end)
 end
 
