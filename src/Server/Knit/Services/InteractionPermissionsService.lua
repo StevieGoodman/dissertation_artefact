c94f4ff3703fffc17d4f423288da89ce373f85bd
Stevie Goodman <mail@steviegoodman.com>
2024-04-24 17:57:47 +0100

    Fixed bug

--- src/Server/Knit/Services/InteractionPermissionsService.lua
+++ src/Server/Knit/Services/InteractionPermissionsService.lua
@@ -1,23 +1,33 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
+local TableUtil = require(ReplicatedStorage.Packages.TableUtil)
 local Waiter = require(ReplicatedStorage.Packages.WaiterV5)
 
 local service = Knit.CreateService {
     Name = "InteractionPermissions",
 }
 
-function service:getKeycardType(player: Player)
-    local keycard = Waiter.get.child(player.Backpack, "Keycard")
-    or Waiter.get.child(player.Character, "Keycard")
-    return if keycard then keycard:GetAttribute("KeycardType") else nil
+function service:getKeycardTypes(player: Player)
+    local keycards =
+    TableUtil.Extend(
+        Waiter.get.children(player.Backpack, "Keycard"),
+        Waiter.get.children(player.Character, "Keycard")
+    )
+    local keycardTypes = TableUtil.Map(keycards, function(keycard)
+        return keycard:GetAttribute("KeycardType")
+    end)
+    return keycardTypes
 end
 
 function service:checkPermissions(player: Player, instance: Instance)
     if player:GetAttribute("InteractionPermissionsOverride") then
         return true
     else
-        return instance:GetAttribute(`{self:getKeycardType(player)}Authorised`)
+        local keycardTypes = self:getKeycardTypes(player)
+        return TableUtil.Some(keycardTypes, function(keycardType)
+            return instance:GetAttribute(keycardType .. "Authorised")
+        end)
     end
 end
 
