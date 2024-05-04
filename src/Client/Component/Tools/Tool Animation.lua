89065003b60592ac911977a9f33c3e355aeaedda
Stevie Goodman <mail@steviegoodman.com>
2024-05-05 00:10:43 +0100

    Fixed tool animations not stopping when dropped

--- src/Client/Component/Tools/Tool Animation.lua
+++ src/Client/Component/Tools/Tool Animation.lua
@@ -1,3 +1,4 @@
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Component = require(ReplicatedStorage.Packages.Component)
@@ -10,22 +11,46 @@ local component = Component.new {
 function component:Construct()
     assert(self.Instance:IsA("Animation"), "ToolAnimation must be an Animation")
     self.Instance = self.Instance :: Animation
-    self.character = self.Instance:FindFirstAncestorWhichIsA("Tool"):FindFirstAncestorWhichIsA("Model")
-    assert(self.character, "Character not found!")
-    self.humanoid = self.character:FindFirstChildWhichIsA("Humanoid")
-    assert(self.humanoid, "Humanoid not found in character!")
-    self.animator = self.humanoid:FindFirstChildWhichIsA("Animator")
-    assert(self.animator, "Animator not found in humanoid!")
+    
 end
 
 function component:Start()
-    self.track = self.animator:LoadAnimation(self.Instance)
+    self.tool = self.Instance:FindFirstAncestorWhichIsA("Tool")
+    assert(self.tool, "ToolAnimation must be a descendant of a tool")
+    self:tryPlay()
+    self.tool.AncestryChanged:Connect(function()
+        self:tryPlay()
+        self:tryStop()
+    end)
+end
+
+function component:Stop()
+    self:tryStop()
+end
+
+function component:tryPlay()
+    local character = self.tool:FindFirstAncestorWhichIsA("Model")
+    if not character then return end
+    local humanoid = character:FindFirstChildWhichIsA("Humanoid")
+    if not humanoid then return end
+    local animator = humanoid:FindFirstChildWhichIsA("Animator")
+    if not animator then return end
+    self.track = animator:LoadAnimation(self.Instance)
     self.track.Looped = true
     self.track:Play()
 end
 
-function component:Stop()
-    self.track:Stop()
+function component:tryStop()
+    local character = self.tool:FindFirstAncestorWhichIsA("Model")
+    if not character then
+        self.track:Stop()
+        return
+    else
+        local player = Players:GetPlayerFromCharacter(character)
+        if player then return end
+        self.track:Stop()
+        return
+    end
 end
 
 return component
