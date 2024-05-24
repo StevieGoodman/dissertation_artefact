92da51be3b1462be45f62758c22aaff8fa4d5058
Stevie Goodman <mail@steviegoodman.com>
2024-05-24 02:09:42 +0100

    Removed unused service

--- src/Server/Component/AI/PathfindingNavigation.lua
+++ src/Server/Component/AI/PathfindingNavigation.lua
@@ -1,4 +1,3 @@
-local PathfindingService = game:GetService("PathfindingService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Component = require(ReplicatedStorage.Packages.Component)
