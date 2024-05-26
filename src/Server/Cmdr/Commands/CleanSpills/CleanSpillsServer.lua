8e7328e007be97e7db686cb0a85096dce199e403
Stevie Goodman <mail@steviegoodman.com>
2024-05-26 23:51:18 +0100

    Added toggle functionality to SCP-008 container

--- src/Server/Cmdr/Commands/CleanSpills/CleanSpillsServer.lua
+++
@@ -1,8 +0,0 @@
-local ServerScriptService = game:GetService("ServerScriptService")
-
-local SpillComponent = require(ServerScriptService.Component.Labour.Spill)
-
-return function(_, amount: number)
-    local spillsCleaned = SpillComponent.clean(amount)
-    return `Cleaned {spillsCleaned} spills!`
-end
---
+++ src/Server/Component/SCPs/SCP-008/SCP008Container.lua
@@ -0,0 +1,54 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Component = require(ReplicatedStorage.Packages.Component)
+local Waiter = require(ReplicatedStorage.Packages.WaiterV5)
+
+local component = Component.new {
+    Tag = "SCP008Container",
+    Ancestors = { workspace }
+}
+
+function component:Construct()
+    self.particleEffects = Waiter.get.descendants(self.Instance, "ParticleFX")
+    self.open = false
+end
+
+function component:Start()
+    Component.InteractableObject:WaitForInstance(self.Instance, 5)
+    :andThen(function(interactableObject)
+        interactableObject.interacted:Connect(function()
+            self:toggle()
+        end)
+    end)
+    :catch(error)
+    :await()
+end
+
+--[=[
+    Toggles the SCP-008 container, opening it if it is closed and closing it if it is open.
+]=]
+function component:toggle()
+    if self.open then
+        self:setOpen(false)
+    else
+        self:setOpen(true)
+    end
+end
+
+--[=[
+    Sets the open state of the SCP-008 container.
+    @param open boolean -- Whether the container should be open.
+]=]
+function component:setOpen(open: boolean)
+    self.open = open
+    if open then
+        self.Instance:AddTag("SCP008")
+    else
+        self.Instance:RemoveTag("SCP008")
+    end
+    for _, particleEffect in self.particleEffects do
+        particleEffect.Enabled = open
+    end
+end
+
+return component
