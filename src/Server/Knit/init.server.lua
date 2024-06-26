10e45c1e873aa389fa2beb09122c28a029877af6
Stevie Goodman <mail@steviegoodman.com>
2024-06-26 13:22:58 +0100

    Refactored server-side structure

--- src/Server/Knit/init.server.lua
+++ src/Server/Knit/init.server.lua
@@ -2,7 +2,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
-Knit.AddServices(script.Parent.Services)
+Knit.AddServices(script)
 Knit.Start()
-:andThenCall(print, "Knit has successfully started on the server!")
-:catch(error)
+    :andThenCall(print, "Knit has successfully started on the server!")
+    :catch(error)
